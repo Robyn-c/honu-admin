7f0691e8e07f43900b8b2244adfc0b163d4e5fc7
Robyn <scuadra02@gmail.com>
2023-11-25 09:21:45 -0300

    feat: clerk auth

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+export default function SetupPage() {
+  return (
+  <div>
+    <p>This is a protected Route!</p>
+  </div>
+  )
+}
